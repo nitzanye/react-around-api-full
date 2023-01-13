c902a697d254c03b42564a68f5e68c1b11a27c0b
NitzanSmulevici <nitzanye@gmail.com>
2023-01-13 22:42:21 +0200

    change domain app

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -85,8 +85,13 @@ class Api {
 
 const BASE_URL =
   process.env.NODE_ENV === 'production'
-    ? 'https://api.around-nitzan.students.nomoredomainssbs.ru'
+    ? 'https://nitzan-smulevici.art'
     : 'http://localhost:3000';
+
+// const BASE_URL =
+//   process.env.NODE_ENV === 'production'
+//     ? 'https://api.around-nitzan.students.nomoredomainssbs.ru'
+//     : 'http://localhost:3000';
 
 export default new Api({
   baseUrl: BASE_URL,
